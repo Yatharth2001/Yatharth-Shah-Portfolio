6ad17c6faa71ff2dc9d96f487a394349dd2235a2
Disha <dishajadav12402@gmail.com>
2024-04-12 12:11:13 +0530

    added certification links

--- src/components/Accreditations/Accreditations.js
+++ src/components/Accreditations/Accreditations.js
@@ -22,6 +22,7 @@ function Accreditations() {
               isBlog={false}
               title="Introduction to the Internet of Things and Embedded Systems"
               description="Sept 30, 2023 | University of California, Irvine"
+              verification="https://coursera.org/verify/EM5M2GD7TG5J"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -30,6 +31,7 @@ function Accreditations() {
               isBlog={false}
               title="Introduction to Big Data with Spark and Hadoop"
               description="Oct 10, 2023 | IBM"
+              verification="https://coursera.org/verify/2YG2SPFD65FG"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -38,6 +40,8 @@ function Accreditations() {
               isBlog={false}
               title="Cryptography"
               description="Oct 5, 2023 | University of Maryland, College Park"
+              verification="https://coursera.org/verify/MS5GX6R42R9Z
+"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -46,14 +50,7 @@ function Accreditations() {
               isBlog={false}
               title="Android App Components - Intents, Activities, and Broadcast Receivers"
               description="Oct 2, 2023 | Vanderbilt University"
-            />
-          </Col>{" "}
-          <Col md={6} className="certificate-card">
-            <AccreditationCard
-              imgPath={iframe}
-              isBlog={false}
-              title="Introduction to Artificial Intelligence (AI)"
-              description="Apr 12, 2023 | IBM"
+              verification="https://coursera.org/verify/GXKGKBD8QBXA"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -62,6 +59,7 @@ function Accreditations() {
               isBlog={false}
               title="Machine Learning with Python"
               description="Apr 10, 2023 | IBM"
+              verification="https://coursera.org/verify/58R2ZYW3VFGW"
             />
           </Col>
           <Col md={6} className="certificate-card">
@@ -70,6 +68,7 @@ function Accreditations() {
               isBlog={false}
               title="Real-Time Cyber Threat Detection and Mitigation"
               description="Apr 10, 2023 | New York University"
+              verification="https://coursera.org/verify/R74LCJB785SN"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -78,6 +77,7 @@ function Accreditations() {
               isBlog={false}
               title="Introduction to DevOps"
               description="Apr 9, 2023 | IBM"
+              verification="https://coursera.org/verify/JURW3QVCS7S8"
             />
           </Col>
           <Col md={6} className="certificate-card">
@@ -86,6 +86,7 @@ function Accreditations() {
               isBlog={false}
               title="Smart Cities – Management of Smart Urban Infrastructures"
               description="Mar 21, 2023 | École Polytechnique Fédérale de Lausanne"
+              verification="https://coursera.org/verify/HT3PLHSK7TBP"
             />
           </Col>
           <Col md={6} className="certificate-card">
@@ -94,7 +95,7 @@ function Accreditations() {
               isBlog={false}
               title="Interactivity with JavaScript"
               description="Nov 9, 2022 | University of Michighan"
-              verification="https://www.coursera.org/account/accomplishments/verify/RNVFJWP87MXS"
+              verification="https://coursera.org/verify/RNVFJWP87MXS"
             />
           </Col>
           <Col md={6} className="certificate-card">
@@ -103,6 +104,7 @@ function Accreditations() {
               isBlog={false}
               title="TCP/IP and Advanced Topics"
               description="Aug 15, 2022 | University of Colorado"
+              verification="https://coursera.org/verify/7HUS3863M4K8"
             />
           </Col>{" "}
           <Col md={6} className="certificate-card">
@@ -110,7 +112,8 @@ function Accreditations() {
               imgPath={iframe}
               isBlog={false}
               title="Programming for Everybody (Getting Started with Python)"
-              description="Aug 12, 2022 | IBM"
+              description="Aug 12, 2022 | University of Michigan"
+              verification="https://coursera.org/verify/GLRH2XEG8FTU"
             />
           </Col>
           <Col md={6} className="certificate-card">
@@ -119,6 +122,7 @@ function Accreditations() {
               isBlog={false}
               title="Initiating and Planning Projects"
               description="Aug 2, 2022 | University of California, Irvine"
+              verification="https://coursera.org/verify/LRUCDNUJK3TZ"
             />
           </Col>{" "}
         </Row>
